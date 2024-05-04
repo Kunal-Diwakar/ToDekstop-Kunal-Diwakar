867d25aeb7039fcb595d7af1a087a0a28d21d16d
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-05-04 13:32:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu() {
+    navDialog.classList.toggle('hidden');
+}
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+
+    intersectionObserver.observe(element);
+
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+
+}
+
+const line1 = document.getElementById('line-1');
+const line2 = document.getElementById('line-2');
+const line3 = document.getElementById('line-3');
+const line4 = document.getElementById('line-4');
+
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4,false,0.7);
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "head": ["Poppins", "sans-serif"],
+        "body": ["Inter", "sans-serif"],
+      },
+  
+      colors: {
+        "primary": "#3238f2",
+      },
+    },
+  },
+  plugins: [],
+}
+
