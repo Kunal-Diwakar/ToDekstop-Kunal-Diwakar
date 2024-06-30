188e451aa3c102711cebd70d15fa53343e658c57
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-06-30 19:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,16 @@ dtElements.forEach(element => {
         ddArrowIcon.classList.toggle('-rotate-180');
     })
 })
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
